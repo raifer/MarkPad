ca5ed92f77533bc6d4c4a145e16ea2d1af43805f
raifer <mat.barbe@gmail.com>
2016-09-25 12:36:14 +0200

    First version with new architecture but specific does not work

--- __init__.py
+++ __init__.py
@@ -27,19 +27,26 @@ def get_accelerator(action):
     return shortkey[action][0]
 # end def
 
-def load_markpad(mul):
+def load_markpad(page):
     """Load MarkPad module"""
     
+    log.h2('Load Markpad with %s language' % page.mul.name)
+    # Compile regex
+    page.mul.compile_regex()
+    # Create goto functions
+    page.mul.generate_goto_functions()
     # Creating MarkUp menu
-    menu_markup = win.menus.add(label = "MarkUp", action = None, index = -2, submenu = True, name = 'markup', specific = True)
+    log('Create MarkPad menu')
+    menu_markup = win.menus.add(label = "MarkPad", action = None, index = -2, submenu = True, name = 'markpad', specific = False)
     # creat sub menu and global submenus and items
-    submenus, items = creat_submenu(menu_markup)
+    log('creat sub menu and global submenus and items')
+    submenus, items = creat_submenu(menu_markup, page.mul.goto)
     # Accelerator
     #accelerator_active = creatAccelerator(shortkey, win)
     return menu_markup, submenus, items
 # end def
 
-def creat_submenu(menu_markup):
+def creat_submenu(menu_markup, goto):
     """Creat sub menu and item for MarkPad modul
     arg : menu_markup, 6pad menu typ
     return submenus dico, items dico"""
@@ -54,7 +61,7 @@ def creat_submenu(menu_markup):
     # items in Goto sub menu
     # next head
     items['next_head'] = submenus['goto'].add(
-    label = msg('Next head'), action = lambda:goto.next_item(HEAD), accelerator = get_accelerator('next_head'), name = "next_head")
+    label = msg('Next head'), action = lambda: goto.next_item(HEAD), accelerator = get_accelerator('next_head'), name = "next_head")
 # Previous head
     items['previous_head'] = submenus['goto'].add(
     label = msg('Previous head'), action = lambda:goto.previous_item(HEAD), accelerator = get_accelerator('previous_head'), name = "previous_head")
@@ -84,11 +91,13 @@ def creat_accelerator(shortkey, win):
 
 def load_translate_file():
     """Load the lang file to translate markpad"""
+    log.h1('Load translate file')
     # Load language
     for lang in(sp.locale, 'english'):
         lang_file = os.path.join(PLUGIN_PATH, lang + '.lng')
         if os.path.isfile(lang_file) :
             # Lang file found
+            log('Lang file found : %s' % lang)
             sp.loadTranslation(lang_file)
         # end if
     # end for
@@ -103,6 +112,8 @@ class MarkupManager():
     
     def __init__(self, plugin_path):
         """Load markdown language definition"""
+        
+        log.h1('Markup Manager init')
         # Load markup language definition from language directory
         self.markup_languages = self._fetch_markup_definition(plugin_path)
         # Extract extensions capability from each markup language
@@ -111,7 +122,8 @@ class MarkupManager():
 
     def _fetch_markup_definition(self, plugin_path):
         """Create MUL file dictionary from file present into "markup language" directory"""
-        
+
+        log.h2('Scan markup language directory')        
         # Instanciate mul dictionary
         markup_langs = {}
         # List mul file
@@ -125,16 +137,25 @@ class MarkupManager():
                 # fetch language name
                 mul_name = file_name[:-4]
                 mul_path = os.path.join(lang_path, file_name)
+                log.h3('%s.mul' % mul_name) 
                 # Add this markup language into markup definition dictionary
-                markup_langs[mul_name] = self._parse_mul_file(mul_name, mul_path)
+                parse_result = self._parse_mul_file(mul_name, mul_path)
+                # Test parsing result.
+                if parse_result:
+                    log('"%s" language added in markup manager' % mul_name)
+                    markup_langs[mul_name] = parse_result
             # end if # mul file found
         # end for # file
+        log('Scan MUL file completed')
         return markup_langs
     # end def
     
     def _parse_mul_file(self, mul_name, mul_path):
         """Parse mul file and return MarkupLanguage definition""" 
+        
+        log('Parsing MUL file "%s"' % mul_name)
         # Open file.
+        log('Open file "%s"' % mul_path)
         mul_file = open(mul_path, encoding='utf8')
         # Instanciate MarkupLanguage class
         mul = MarkupManager.MarkupLanguage(mul_name, mul_path)
@@ -156,44 +177,50 @@ class MarkupManager():
                 # find key type, extension, help or regex
                 if 'extension' in key:
                     # Value is a list of extension compatible
+                    log('Extensions declared : ' + str(value)[1:-1])
                     mul.extension = value
                 elif 'help' in key:
+                    # Help page definition
+                    log('Help link : ' + value)
                     mul.help = value
                 else:
                     # Regex definition
+                    log('%s, regex : "%s"' %(key, value))
                     # Convert item type 
                     key = eval(key)
                     mul[key] = value
                 # end if
             except ValueError : 
-                print('Error during MarkPad load Markup language definition\nFile : %s\nSyntaxe error at line %d\n%s' %(mul_path, n_line, line))
-                return -1
+                win.warning('Parsing MUL file error.\nFile : "%s"\nSyntaxe error at line %d\n%s' %(mul_path, n_line, line), 'Markpad Error')
+                log('Error at line %d : %s' %( n_line, line))
+                return None
             # end try
-            # end except
-            
         # end for
+        log('Close MUL file')
         mul_file.close()
         return mul
     # end def
     
     def _extract_capabilitys(self, markup_languages):
-        """From markup languages dico, return capability dictionary
-        
+        """From MarkupManager containing MUL definition, extract markpad capabilities
         @ markup_languages, dictionary containing mul dico;
         return capability, dictionary. [ext]:"markup language name"
         """
         
+        log.h2('Extract Markpad capabilities')
         # Instanciate capability dictionary
         caps = {}
         # Extract extension from each language
         for mul in markup_languages.values():
-            # Test Verify extension definition.
+            # Verify extension definition.
             if not mul.extension:
-                print('Warning, no "extension" definition found in "%s" markup language' % mul.name)
+                log('Warning, no "extension" definition found in "%s" markup language' % mul.name)
+                win.warning('No extension found into "%s" markup language definition. This language will be ignored.' % mul.name, 'Markpad warning')
                 # next language
                 continue
             # end if
             # explore each extension and add it in the capability dictionary with its language
+            log('"%s" extensions :\n%s' %(mul.name, str(mul.extension)[1:-1]))
             for ext in mul.extension:
                 caps[ext] = mul.name
             # end for # , ext
@@ -213,50 +240,71 @@ class MarkupManager():
         def compile_regex(self):
             if self._compiled:
                 # Regex already compiled
+                log('Regex already compiled')
                 return
             # end if
             # Compile regex
             for item_type, regex_raw in self.items():
-                mul[item_type] = re.compile(regex_raw, re.MULTILINE)
+                log('Compile %s' % item_type)
+                self[item_type] = re.compile(regex_raw, re.MULTILINE)
             # end for
+            log('Compile completed')
             self._compiled = True
         # end def
         
-        def create_goto_fonction(self):
-            self.goto = goto_regex.Goto(sp, self)
+        def generate_goto_functions(self):
+            # Verify is goto function have not already been created
+            if hasattr(self, "goto"):
+                log('Gotot functions alredy generated') 
+            else:
+                log('Creat goto function for this language')
+                self.goto = goto_regex.Goto(self)
         # end def
     # end class MarkupLanguage
 # end class MarkupManager
 
 def page_opened(page):
+    """Start when new page is openned.
+    find file extension and load markPad if a markup language uses this extension""" 
+    
     # fetch extension of this new page
+    log.h1('Page "%s" opened, search kind of this document' % page.name)
     ext = os.path.splitext(page.file)[1][1:].lower()
     if not ext:
+        # This page contains a new file or file without extension.
+        log('Page not saved page or file without extension.')
         ext = 'noext'
+    # end if
+    log('File extension : %s' % ext)
     # Search language for this extension
-    if ext in markup_manager.caps.key:
+    if ext in markup_manager.caps:
         # MUL available for this extension
-        mul_name = mul.caps[ext]
-        # Load MarkupLanguage 
+        mul_name = markup_manager.caps[ext]
+        log('Markup language found : "%s".' %mul_name)
+        # Add MUL reference in page
         page.mul = markup_manager.markup_languages[mul_name]
-        page.mul.goto, menu_markup, submenus, items = load_module("markdown")
+        # Load module markPad
+        menu_markup, submenus, items = load_markpad(page)
+    else:
+        log('No markup language found for this kind of document')
+        log('Markpad not loaded for "%s".' % page.name)
+    # end else
 # end def
 
 ## main ##
 # Init log systeme
 log_file_path = os.path.join(PLUGIN_PATH, 'log_markpad.md')
 log = tracer.Tracer(log_file_path)
-log.h1('MarkPad log')
+log.h1('MarkPad trace')
 log.print_time()
 
 # Load lng file to translate markpad
 load_translate_file()
 
 # Init Markup Manager.
-# Scan MUL files to find languages and extract compatible extension list.
-markup_m = MarkupManager(PLUGIN_PATH)
+markup_manager = MarkupManager(PLUGIN_PATH)
 
+# Add event to load module when new page is open
 win.addEvent('pageOpened', page_opened)
-
-#menu_markup, submenus, items = load_markpad("markdown")
-print("fin du main")
+# Load module for the initial page
+page_opened(win.curPage)
